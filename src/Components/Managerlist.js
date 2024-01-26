b18e485dd26f941748369615b87a0b67d286781f
sharaf777 <64193913+sharaf777@users.noreply.github.com>
2024-01-26 22:06:59 +0530

    Update Managerlist.js

--- src/Components/Managerlist.js
+++ src/Components/Managerlist.js
@@ -48,21 +48,22 @@ function Managerlist() {
 
         <main className="leaderboard__profiles">
          {loading ? (
-            <p>Loading...</p>
-          ) : (
-            projectManagers && projectManagers.length > 0 ? (
-              projectManagers.map((manager) => (
-                <article key={manager._id} className="leaderboard__profile">
-                 <div className="">
-                        <AccountCircleIcon />
-                 </div>
-                 <span className="leaderboard__name">{manager.username}</span>
-                </article>
-              ))
-            ) : (
-              <p>No project managers available!</p>
-            )
-          )}
+  <p>Loading...</p>
+) : (
+  projectManagers && projectManagers.length > 0 ? (
+    projectManagers.map((manager) => (
+      <article key={manager._id} className="leaderboard__profile">
+        <div className="">
+          <AccountCircleIcon />
+        </div>
+        <span className="leaderboard__name">{manager.username}</span>
+      </article>
+    ))
+  ) : (
+    <p>No project managers available!</p>
+  )
+)}
+
 
 
         </main>
